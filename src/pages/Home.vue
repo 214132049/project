8a73e1f2741d341e8f0ab4a6c8caa8e2f8acd292
li <wenli.li@ymm56.com>
2018-09-26 19:26:36 +0800

    添加加载更多组件

---
+++ src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <scroll class="wrapper" :data="data" :pulldown="pulldown" @pulldown="loadData">
+    <ul class="content">
+      <li v-for="(item, index) in data" :key="index">{{item}}</li>
+    </ul>
+    <div class="loading-wrapper"></div>
+  </scroll>
+</template>
+<script>
+  import BScroll from 'better-scroll'
+  export default {
+    data() {
+      return {
+        data: [],
+        pulldown: true
+      }
+    },
+    created() {
+      this.loadData()
+    },
+    methods: {
+      loadData() {
+        requestData().then((res) => {
+          this.data = res.data.concat(this.data)
+        })
+      }
+    }
+  }
+</script>
